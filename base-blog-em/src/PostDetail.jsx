eb9cbdb24e6994272975e9e65e5e9ad1d1f37269
Dennis <okoyedennis7@gmail.com>
2023-06-16 15:40:17 +0100

    initial commit

---
+++ base-blog-em/src/PostDetail.jsx
@@ -0,0 +1,83 @@
+import { useMutation, useQuery } from "react-query";
+
+async function fetchComments(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  );
+  return response.json();
+}
+
+async function deletePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "DELETE" }
+  );
+  return response.json();
+}
+
+async function updatePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+  );
+  return response.json();
+}
+
+export function PostDetail({ post }) {
+  // replace with useQuery
+  const { data, isLoading, error, isError } = useQuery(
+    ["comments", post.id],
+    () => fetchComments(post.id)
+  );
+
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+
+  const updateMutation = useMutation((postId) => updatePost(postId));
+
+  if (isLoading) return <h3>Loading...</h3>;
+
+  if (isError) {
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && (
+        <p style={{ color: "red" }}>Error deleting the post</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: "purple" }}>Deleting the post</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>Post has deleted successfully</p>
+      )}
+
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updateMutation.isError && (
+        <p style={{ color: "red" }}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: "purple" }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: "green" }}>Post has updated successfully</p>
+      )}
+      <p>{post.body}</p>
+      <h4>Comments</h4>
+      {data.map((comment) => (
+        <li key={comment.id}>
+          {comment.email}: {comment.body}
+        </li>
+      ))}
+    </>
+  );
+}
---
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -0,0 +1,50 @@
+import InfiniteScroll from "react-infinite-scroller";
+import { Species } from "./Species";
+import { useInfiniteQuery } from "react-query";
+
+const initialUrl = "https://swapi.dev/api/species/";
+const fetchUrl = async (url) => {
+  const response = await fetch(url);
+  return response.json();
+};
+
+export function InfiniteSpecies() {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery(
+    "sw-species",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+    );
+  
+   if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div className="loading">Error! {error.toString()}</div>;
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return (
+              <Species
+                key={person.name}
+                name={person.name}
+                language={person.language}
+                averageLifespan={person.average_lifespan}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+      ;
+    </>
+  );
+}
